2c88281a4fb39cc4e48f29ed05756ad3c08f5685
neerajnarayanan <neerajperiya@gmail.com>
2020-06-08 11:15:19 -0700

    Updates

---
+++ precache-manifest.67b08eb022fde5c4510ac5236ae8c6ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6234288f0e7caaaac3e0",
+    "url": "/redux/static/css/main.716a9337.chunk.css"
+  },
+  {
+    "revision": "6234288f0e7caaaac3e0",
+    "url": "/redux/static/js/main.6234288f.chunk.js"
+  },
+  {
+    "revision": "a08fa69e5c50ee11e363",
+    "url": "/redux/static/js/1.a08fa69e.chunk.js"
+  },
+  {
+    "revision": "b0b4bc0870d3c948c5a0",
+    "url": "/redux/static/js/runtime~main.b0b4bc08.js"
+  },
+  {
+    "revision": "d0bbf85c8137c731066c6beeab1b842c",
+    "url": "/redux/index.html"
+  }
+];
